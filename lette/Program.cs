75b9ad061c58f488389cf0faf0bf75f42fcf4446
dagothig <guillaumenoelmartel@gmail.com>
2021-04-04 20:07:22 -0400

    Retrait du shit de test des anims flags

--- lette/Program.cs
+++ lette/Program.cs
@@ -8,20 +8,6 @@ namespace Lette
         [STAThread]
         static void Main()
         {
-            Flags<AnimFlag> flags = new Flags<AnimFlag>();
-            flags[AnimFlag.DirL] = true;
-            if (!flags[AnimFlag.DirL] || flags[AnimFlag.Moving])
-                throw new Exception("BANANA");
-            flags[AnimFlag.DirR] = true;
-            if (!flags[AnimFlag.DirL] || flags[AnimFlag.Moving] || !flags[AnimFlag.DirR])
-                throw new Exception("BANANA");
-            if (!flags.Matches(Flags<AnimFlag>.New(AnimFlag.DirL, AnimFlag.DirR)))
-                throw new Exception("BANANA");
-            ulong test = 1 | 1 << 16;
-            test |= 1 << 1;
-            test &= ~(11UL);
-            if (test != 1 << 16)
-                throw new Exception("BANANA");
             using (var game = new CossinLette())
             {
                 game.Run();
